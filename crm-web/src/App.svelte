7a7509917361429f7a6afb1a8ba3615df371f4e5
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-01-23 18:53:27 +0800

    add NavigationDrawer.svelte

--- crm-web/src/App.svelte
+++ crm-web/src/App.svelte
@@ -3,6 +3,7 @@ import Tailwindcss from './Tailwindcss.svelte';
 import Button from './widgets/Button.svelte';
 import Input from './widgets/Input.svelte';
 import Autocomplete from './widgets/Autocomplete.svelte';
+import NavigationDrawer from './widgets/NavigationDrawer.svelte';
 import { countries } from './countries';
 
 export let name = '';
@@ -20,6 +21,8 @@ let fruit = '';
 function countryChanged(item) {
   console.log(item);
 }
+
+let visible = false;
 </script>
 
 <style>
@@ -29,7 +32,35 @@ function countryChanged(item) {
 </style>
 
 <Tailwindcss/>
-<div class="py-5 px-32 bg-yellow-400">
+<div
+  class="bg-blue-800 fixed left-0 right-0 top-0 h-16 mt-0 z-10 flex items-center justify-between">
+  <div class="flex items-center">
+    <i class="material-icons text-white ml-6 cursor-pointer ripple"
+       on:click={()=>visible=!visible}>
+      menu
+    </i>
+    <span class="ml-4 text-xl font-semibold text-white">Time to Trade</span>
+  </div>
+  <span class="mr-6 text-lg uppercase text-white">Tan Yin Loo</span>
+</div>
+<div class="mb-5 mt-16 px-32">
+  <NavigationDrawer bind:visible={visible} marginTop="mt-16">
+    <div class="w-56 bg-red-100 h-full">
+      <h3 class="font-medium px-6 pb-3 pt-4 tracking-wide text-gray-900">Menu</h3>
+      <ul class="">
+        <li class="px-4 py-3 hover:bg-gray-200 text-gray-800 text-sm tracking-wide">Stock
+          Analysis
+        </li>
+        <li class="px-4 py-3 hover:bg-gray-200 text-gray-800 text-sm tracking-wide">
+          Subscriptions
+        </li>
+      </ul>
+    </div>
+  </NavigationDrawer>
+  <div class="flex flex-row-reverse">
+    <Button text textColor="text-gray-900" on:click='{()=> visible=!visible}'>Toggle
+    </Button>
+  </div>
   <h1>Hello {countrySelected.name?countrySelected.name:'No country selected'}!</h1>
   <Button textColor="text-white" bgColor="bg-orange-500">Normal Button</Button>
   <Button textColor="text-orange-500" outlineColor="border-orange-500" outlined rounded>
