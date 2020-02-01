6b7bc5a0dab71558a796bccd8b0192ac8511b9d9
tianhai82 <tianhai82@yahoo.com.sg>
2020-02-01 10:10:43 +0800

    implement slider

--- crm-web/src/App.svelte
+++ crm-web/src/App.svelte
@@ -1,31 +1,32 @@
 <script>
-import Tailwindcss from './Tailwindcss.svelte';
-import Button from './widgets/Button.svelte';
-import Input from './widgets/Input.svelte';
-import Autocomplete from './widgets/Autocomplete.svelte';
-import NavigationDrawer from './widgets/NavigationDrawer.svelte';
-import Dialog from './widgets/Dialog.svelte';
-import Slider from './widgets/Slider.svelte';
-import { countries } from './countries';
+  import Tailwindcss from "./Tailwindcss.svelte";
+  import Button from "./widgets/Button.svelte";
+  import Input from "./widgets/Input.svelte";
+  import Autocomplete from "./widgets/Autocomplete.svelte";
+  import NavigationDrawer from "./widgets/NavigationDrawer.svelte";
+  import Dialog from "./widgets/Dialog.svelte";
+  import Slider from "./widgets/Slider.svelte";
+  import { countries } from "./countries";
 
-export let name = '';
-let countrySelected = {};
-let error = '';
-$: if (name.trim().length === 0) {
-  error = 'Please enter a name';
-} else {
-  error = '';
-}
+  export let name = "";
+  let countrySelected = {};
+  let error = "";
+  let sliderValue = 35;
+  $: if (name.trim().length === 0) {
+    error = "Please enter a name";
+  } else {
+    error = "";
+  }
 
-const fruits = ['apple', 'orange', 'pear', 'strawberry'];
-let fruit = '';
+  const fruits = ["apple", "orange", "pear", "strawberry"];
+  let fruit = "";
 
-function countryChanged(item) {
-  console.log(item);
-}
+  function countryChanged(item) {
+    console.log(item);
+  }
 
-let visible = false;
-let dialogVisible = false;
+  let visible = false;
+  let dialogVisible = false;
 </script>
 
 <style>
@@ -34,12 +35,14 @@ let dialogVisible = false;
   }
 </style>
 
-<Tailwindcss/>
+<Tailwindcss />
 <div
-  class="bg-blue-800 fixed left-0 right-0 top-0 h-16 mt-0 z-10 flex items-center justify-between">
+  class="bg-blue-800 fixed left-0 right-0 top-0 h-16 mt-0 z-10 flex items-center
+  justify-between">
   <div class="flex items-center">
-    <i class="material-icons text-white ml-6 cursor-pointer ripple"
-       on:click={()=>visible=!visible}>
+    <i
+      class="material-icons text-white ml-6 cursor-pointer ripple"
+      on:click={() => (visible = !visible)}>
       menu
     </i>
     <span class="ml-4 text-xl font-semibold text-white">Time to Trade</span>
@@ -47,73 +50,138 @@ let dialogVisible = false;
   <span class="mr-6 text-lg uppercase text-white">Tan Yin Loo</span>
 </div>
 <div class="mb-5 mt-16 lg:px-32 px-4">
-  <NavigationDrawer bind:visible={visible} marginTop="mt-16">
+  <NavigationDrawer bind:visible marginTop="mt-16">
     <div class="w-56 bg-red-100 h-full">
-      <h3 class="font-medium px-6 pb-3 pt-4 tracking-wide text-gray-900">Menu</h3>
+      <h3 class="font-medium px-6 pb-3 pt-4 tracking-wide text-gray-900">
+        Menu
+      </h3>
       <ul class="">
-        <li class="px-4 py-3 hover:bg-gray-200 text-gray-800 text-sm tracking-wide">Stock
-          Analysis
+        <li
+          class="px-4 py-3 hover:bg-gray-200 text-gray-800 text-sm tracking-wide">
+          Stock Analysis
         </li>
-        <li class="px-4 py-3 hover:bg-gray-200 text-gray-800 text-sm tracking-wide">
+        <li
+          class="px-4 py-3 hover:bg-gray-200 text-gray-800 text-sm tracking-wide">
           Subscriptions
         </li>
       </ul>
     </div>
   </NavigationDrawer>
   <div class="flex flex-row-reverse">
-    <Button text textColor="text-gray-900" on:click='{()=> dialogVisible=!dialogVisible}'>
+    <Button
+      text
+      textColor="text-gray-900"
+      on:click={() => (dialogVisible = !dialogVisible)}>
       Toggle
     </Button>
   </div>
-  <h1>Hello {countrySelected.name?countrySelected.name:'No country selected'}!</h1>
+  <Button on:click={() => (sliderValue = 35)}>Set Slider Value to 35</Button>
+  {sliderValue}
+  <Slider
+    min={20}
+    max={80}
+    bind:value={sliderValue}
+    thumbColor="text-red-600"
+    trackEmptyColor="bg-red-200"
+    trackFilledColor="bg-red-600" />
+  <h1>
+    Hello {countrySelected.name ? countrySelected.name : 'No country selected'}!
+  </h1>
   <Button textColor="text-white" bgColor="bg-orange-500">Normal Button</Button>
-  <Button textColor="text-orange-500" outlineColor="border-orange-500" outlined rounded>
+  <Button
+    textColor="text-orange-500"
+    outlineColor="border-orange-500"
+    outlined
+    rounded>
     Normal Button
   </Button>
   <Button textColor="text-orange-500" text>Normal Button</Button>
   <div>fruit: {fruit}</div>
-  <Autocomplete borderColor="border-green-600" labelColor="text-red-700" label="Nameol"
-                bind:value="{fruit}"
-                items="{fruits}"/>
-  <!-- <Slider/> -->
-  <Input borderColor="border-green-600" labelColor="text-red-700" label="Namewertyu uiou"
-         icon="search"
-         helperText={error} helperTextColor="text-red-500" bind:value={name}/>
-  <Button textColor="text-white" bgColor="bg-orange-500" rounded>Normal Button</Button>
+  <Autocomplete
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="Nameol"
+    bind:value={fruit}
+    items={fruits} />
+  <Input
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="Namewertyu uiou"
+    icon="search"
+    helperText={error}
+    helperTextColor="text-red-500"
+    bind:value={name} />
+  <Button textColor="text-white" bgColor="bg-orange-500" rounded>
+    Normal Button
+  </Button>
   {name} {error}
-  <Input outlined borderColor="border-green-600" labelColor="text-red-700"
-         label="Namewert"
-         icon="search"
-         helperText={error} helperTextColor="text-red-500" bind:value={name}/>
-  <Input outlined borderColor="border-green-600" labelColor="text-red-700"
-         label="Namewert Country here is very long"
-         icon="search"
-         helperText={error} helperTextColor="text-red-500" bind:value={name}/>
+  <Input
+    outlined
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="Namewert"
+    icon="search"
+    helperText={error}
+    helperTextColor="text-red-500"
+    bind:value={name} />
+  <Input
+    outlined
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="Namewert Country here is very long"
+    icon="search"
+    helperText={error}
+    helperTextColor="text-red-500"
+    bind:value={name} />
   <Dialog bind:visible={dialogVisible} permanent>
     <div class="p-6 bg-gray-100 w-64 rounded">
       <div class="flex flex-col">
-      <div class="mb-4">Huat lah!!!</div>
-      <Button on:click={()=>dialogVisible=false} outlined outlineColor="border-green-600" textColor="text-green-600">Close</Button>
+        <div class="mb-4">Huat lah!!!</div>
+        <Button
+          on:click={() => (dialogVisible = false)}
+          outlined
+          outlineColor="border-green-600"
+          textColor="text-green-600">
+          Close
+        </Button>
       </div>
     </div>
   </Dialog>
-  <Autocomplete borderColor="border-green-600" labelColor="text-red-700" label="Nameol"
-                bind:value="{countrySelected}"
-                items="{countries}"
-                keywordsFunction="{it=>`${it.name.toLowerCase()}|^|${it.code.toLowerCase()}`}"
-                labelFieldName="name" minCharactersToSearch={1}
-                on:change={countryChanged}
-                outlined/>
+  <Autocomplete
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="Nameol"
+    bind:value={countrySelected}
+    items={countries}
+    keywordsFunction={it => `${it.name.toLowerCase()}|^|${it.code.toLowerCase()}`}
+    labelFieldName="name"
+    minCharactersToSearch={1}
+    on:change={countryChanged}
+    outlined />
 
   <!--    <Input borderColor="border-green-600" labelColor="text-red-700" label="Name"-->
   <!--           helperText={error} helperTextColor="text-red-500" bind:value={name}/>-->
 
-  <Button textColor="text-white" bgColor="bg-orange-500" rounded>Normal Button</Button>
+  <Button textColor="text-white" bgColor="bg-orange-500" rounded>
+    Normal Button
+  </Button>
   {name} {error}
-  <Input outlined borderColor="border-green-600" labelColor="text-red-700" label="b"
-         icon="search"
-         helperText={error} helperTextColor="text-red-500" bind:value={name}/>
-  <Input outlined borderColor="border-green-600" labelColor="text-red-700" label="Name"
-         icon="search"
-         helperText={error} helperTextColor="text-red-500" bind:value={name}/>
+  <Input
+    outlined
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="b"
+    icon="search"
+    helperText={error}
+    helperTextColor="text-red-500"
+    bind:value={name} />
+  <Input
+    outlined
+    borderColor="border-green-600"
+    labelColor="text-red-700"
+    label="Name"
+    icon="search"
+    helperText={error}
+    helperTextColor="text-red-500"
+    bind:value={name} />
 </div>
--- crm-web/src/widgets/Slider.svelte
+++ crm-web/src/widgets/Slider.svelte
@@ -1,43 +1,139 @@
+<script>
+  export let value;
+  export let min = 0;
+  export let max = 1;
+  export let step = undefined;
+  export let thumbColor = "text-blue-500";
+  export let trackEmptyColor = "bg-blue-200";
+  export let trackFilledColor = "bg-blue-500";
+
+  let normalisedValue;
+  $: normalisedValue = scaleValue(value, min, max, 0, 1);
+  // $: value = scaleValue(normalisedValue, 0, 1, min, max);
+
+  let width;
+  let container;
+  let oldVal;
+  let dragStartX;
+  let mousedown = false;
+
+  function scaleValue(v, oldMin, oldMax, newMin, newMax) {
+    if (v < oldMin) {
+      return newMin;
+    }
+    if (v > oldMax) {
+      return newMax;
+    }
+    const oldRange = oldMax - oldMin;
+    const newRange = newMax - newMin;
+    if (oldRange <= 0 || newRange <= 0) {
+      throw new Error("max should be greater than min");
+    }
+    return ((v - oldMin) * newRange) / oldRange + newMin;
+  }
+
+  function touchStart(e) {
+    if (window.PointerEvent) {
+      return;
+    }
+    const rect = container.getBoundingClientRect();
+    const x = e.touches[0].clientX - rect.left;
+    const v = x / width;
+    if (v < 0) {
+      normalisedValue = 0;
+    } else if (v > 1) {
+      normalisedValue = 1;
+    } else {
+      normalisedValue = v;
+    }
+    dragStartX = e.touches[0].screenX;
+    oldVal = normalisedValue;
+    value = scaleValue(normalisedValue, 0, 1, min, max);
+    mousedown = true;
+  }
+  function touchMove(e) {
+    if (window.PointerEvent) {
+      return;
+    }
+    if (!mousedown) {
+      return;
+    }
+    const change = e.touches[0].screenX - dragStartX;
+    const v = change / width + oldVal;
+    if (v < 0) {
+      normalisedValue = 0;
+    } else if (v > 1) {
+      normalisedValue = 1;
+    } else {
+      normalisedValue = v;
+    }
+    value = scaleValue(normalisedValue, 0, 1, min, max);
+  }
+
+  function dragStart(e) {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left; //x position within the element.
+    const v = x / width;
+    if (v < 0) {
+      normalisedValue = 0;
+    } else if (v > 1) {
+      normalisedValue = 1;
+    } else {
+      normalisedValue = v;
+    }
+    dragStartX = e.screenX;
+    oldVal = normalisedValue;
+    mousedown = true;
+    value = scaleValue(normalisedValue, 0, 1, min, max);
+  }
+  function dragging(e) {
+    if (e.pressure === 0) {
+      mousedown = false;
+      return;
+    }
+    if (!mousedown) {
+      return;
+    }
+    const change = e.screenX - dragStartX;
+    const v = change / width + oldVal;
+    if (v < 0) {
+      normalisedValue = 0;
+    } else if (v > 1) {
+      normalisedValue = 1;
+    } else {
+      normalisedValue = v;
+    }
+    value = scaleValue(normalisedValue, 0, 1, min, max);
+  }
+  function dragEnd(e) {
+    mousedown = false;
+  }
+
+  let thumbSize = 0.75;
+  $: thumbSize = mousedown ? 1.4 : 0.75;
+</script>
+
 <style>
   .mdc-slider {
-    position: relative;
-    width: 100%;
-    height: 48px;
-    cursor: pointer;
-    -ms-touch-action: pan-x;
-    touch-action: pan-x;
+    -ms-touch-action: none;
+    touch-action: none;
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   }
 
-  .mdc-slider:not(.mdc-slider--disabled) .mdc-slider__track-container {
-    background-color: rgba(1, 135, 134, .26);
-  }
-
   .mdc-slider__track-container {
-    position: absolute;
     top: 50%;
-    width: 100%;
     height: 2px;
     overflow: hidden;
   }
-
-  .mdc-slider:not(.mdc-slider--disabled) .mdc-slider__track {
-    background-color: #018786;
-  }
-
   .mdc-slider__track {
-    position: absolute;
-    width: 100%;
-    height: 100%;
     -webkit-transform-origin: left top;
     -ms-transform-origin: left top;
     transform-origin: left top;
     will-change: transform;
   }
-
   .mdc-slider__thumb-container {
     position: absolute;
-    top: 15px;
+    margin-top: 7px;
     left: 0;
     width: 21px;
     height: 100%;
@@ -49,43 +145,65 @@
   }
 
   .mdc-slider__thumb {
-    position: absolute;
-    top: 0;
-    left: 0;
-    -webkit-transform: scale(.571);
-    -ms-transform: scale(.571);
-    transform: scale(.571);
     stroke-width: 3.5;
-    -webkit-transition: fill .1s ease-out, stroke .1s ease-out, -webkit-transform .1s ease-out;
-    transition: fill .1s ease-out, stroke .1s ease-out, -webkit-transform .1s ease-out;
-    -o-transition: transform .1s ease-out, fill .1s ease-out, stroke .1s ease-out;
-    transition: transform .1s ease-out, fill .1s ease-out, stroke .1s ease-out;
-    transition: transform .1s ease-out, fill .1s ease-out, stroke .1s ease-out, -webkit-transform .1s ease-out;
+    -webkit-transition: fill 0.1s ease-out, stroke 0.1s ease-out,
+      -webkit-transform 0.1s ease-out;
+    transition: fill 0.1s ease-out, stroke 0.1s ease-out,
+      -webkit-transform 0.1s ease-out;
+    -o-transition: transform 0.1s ease-out, fill 0.1s ease-out,
+      stroke 0.1s ease-out;
+    transition: transform 0.1s ease-out, fill 0.1s ease-out,
+      stroke 0.1s ease-out;
+    transition: transform 0.1s ease-out, fill 0.1s ease-out,
+      stroke 0.1s ease-out, -webkit-transform 0.1s ease-out;
   }
-
   .mdc-slider__focus-ring {
     width: 21px;
     height: 21px;
     border-radius: 50%;
-    opacity: 0;
-    -webkit-transition: opacity .26667s ease-out, background-color .26667s ease-out, -webkit-transform .26667s ease-out;
-    transition: opacity .26667s ease-out, background-color .26667s ease-out, -webkit-transform .26667s ease-out;
-    -o-transition: transform .26667s ease-out, opacity .26667s ease-out, background-color .26667s ease-out;
-    transition: transform .26667s ease-out, opacity .26667s ease-out, background-color .26667s ease-out;
-    transition: transform .26667s ease-out, opacity .26667s ease-out, background-color .26667s ease-out, -webkit-transform .26667s ease-out;
+    -webkit-transition: opacity 0.26667s ease-out,
+      background-color 0.26667s ease-out, -webkit-transform 0.26667s ease-out;
+    transition: opacity 0.26667s ease-out, background-color 0.26667s ease-out,
+      -webkit-transform 0.26667s ease-out;
+    -o-transition: transform 0.26667s ease-out, opacity 0.26667s ease-out,
+      background-color 0.26667s ease-out;
+    transition: transform 0.26667s ease-out, opacity 0.26667s ease-out,
+      background-color 0.26667s ease-out;
+    transition: transform 0.26667s ease-out, opacity 0.26667s ease-out,
+      background-color 0.26667s ease-out, -webkit-transform 0.26667s ease-out;
   }
 </style>
 
-<div class="mdc-slider" tabindex="0" role="slider" aria-valuemin="0" aria-valuemax="100"
-     aria-valuenow="45.135467980295566" aria-label="Select Value">
-  <div class="mdc-slider__track-container">
-    <div class="mdc-slider__track" style="transform: scaleX(0.451355);"></div>
+<div
+  class="relative w-full h-8 cursor-pointer block outline-none mdc-slider"
+  tabindex="0"
+  role="slider"
+  bind:clientWidth={width}
+  bind:this={container}
+  on:touchstart|stopPropagation|preventDefault={touchStart}
+  on:touchmove|stopPropagation|preventDefault={touchMove}
+  on:touchend|stopPropagation|preventDefault={dragEnd}
+  on:pointerdown|stopPropagation|preventDefault={dragStart}
+  on:pointermove|stopPropagation|preventDefault={dragging}
+  on:pointerup|stopPropagation|preventDefault={dragEnd}>
+  <div class="absolute w-full mdc-slider__track-container {trackEmptyColor}">
+    <div
+      class="h-full w-full absolute mdc-slider__track {trackFilledColor}"
+      style="transform: scaleX({normalisedValue});" />
   </div>
-  <div class="mdc-slider__thumb-container"
-       style="transform: translateX(366.5px) translateX(-50%);">
-    <svg class="mdc-slider__thumb" width="21" height="21">
-      <circle cx="10.5" cy="10.5" r="7.875"></circle>
+  <div
+    class="mdc-slider__thumb-container"
+    style="transform: translateX({width * normalisedValue}px) translateX(-50%);">
+    <svg
+      class="absolute left-0 top-0 fill-current {thumbColor} mdc-slider__thumb"
+      style="transform: scale({thumbSize});"
+      width="21"
+      height="21">
+      <circle cx="10.5" cy="10.5" r="7.875" />
     </svg>
-    <div class="mdc-slider__focus-ring"></div>
+    <div
+      style="transform: scale(1.125);"
+      class="mdc-slider__focus-ring {trackFilledColor} hover:opacity-25
+      opacity-0" />
   </div>
 </div>
